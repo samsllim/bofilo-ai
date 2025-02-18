352b9172adf10ef1ee93439a927622be6480d62f
Sam Sllim <61842100+samsllim@users.noreply.github.com>
2025-02-18 11:11:46 +0800

    Update layout.tsx add lhc widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,14 @@ export default function RootLayout({
           </TRPCReactProvider>
           <Toaster />
         </ThemeProvider>
+        <script>var LHC_API = LHC_API||{};
+          LHC_API.args = {mode:'widget',lhc_base_url:'//lhc.bofilo.com/index.php/',wheight:450,wwidth:350,pheight:520,pwidth:500,leaveamessage:true,check_messages:false};
+          (function() {
+          var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin','anonymous'); po.async = true;
+          var date = new Date();po.src = '//lhc.bofilo.com/design/defaulttheme/js/widgetv2/index.js?'+(""+date.getFullYear() + date.getMonth() + date.getDate());
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+          })();
+        </script>
       </body>
     </html>
     </ClerkProvider>
